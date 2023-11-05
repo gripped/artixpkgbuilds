820624fe4e9c94d74ba2a6746aac818008fe748d
gripped <gripped@gripped.com>
2023-11-05 16:57:56 +0000

    Updated cuda - 12.3.0-1

--- world/cuda/cuda.sh
+++ world/cuda/cuda.sh
@@ -1,4 +1,7 @@
 export CUDA_PATH=/opt/cuda
 export PATH=$PATH:/opt/cuda/bin:/opt/cuda/nsight_compute:/opt/cuda/nsight_systems/bin
-# NOTE: Make sure this is updated when the gcc host compiler dependency changes.
-export NVCC_PREPEND_FLAGS='-ccbin /usr/bin/g++-12'
+
+# This line used to not be required but it somehow is with cuda 12.3.
+# We reported this as a bug to NVIDIA. For now, this seems like a viable
+# workaround.
+export NVCC_PREPEND_FLAGS='-ccbin /opt/cuda/bin'
