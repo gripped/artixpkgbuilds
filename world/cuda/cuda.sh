72c924aa12c469eeca17e85ca867c3509e512181
gripped <gripped@gripped.com>
2026-05-07 01:00:31 +0100

    Updated cuda - 13.2.1-3

--- world/cuda/cuda.sh
+++ world/cuda/cuda.sh
@@ -6,4 +6,4 @@ export PATH
 # Set the default host compiler for nvcc. This will need to be switched back
 # and forth between the latest and previous GCC version, whatever nvcc
 # currently supports.
-export NVCC_CCBIN='/usr/bin/g++'
+export NVCC_CCBIN='/usr/bin/g++-15'
