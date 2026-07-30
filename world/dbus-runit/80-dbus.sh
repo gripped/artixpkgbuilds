310856ae695b55cbe55e4f0258823720aa494531
gripped <gripped@gripped.com>
2026-07-30 08:22:54 +0100

    Updated dbus-runit - 20260730-1

--- world/dbus-runit/80-dbus.sh
+++ world/dbus-runit/80-dbus.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# launches a session dbus instance
+# launches a session dbus instance, unless already started by the known user sv
+
+[ -S "${XDG_RUNTIME_DIR:-/run/user/$(id -u)}/bus" ] && return 0
 
 dbuslaunch="$(command -v dbus-launch)"
 if [ -n "$dbuslaunch" ] && [ -x "$dbuslaunch" ] && [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
